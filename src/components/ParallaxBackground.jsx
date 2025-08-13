23b9f1fa2e0c97375c70e589185bc97d777504f2
Akmalalfareza130305 <akmalalfareza20@gmail.com>
2025-08-13 20:54:36 +0700

    menambahkan 3D

--- src/components/ParallaxBackground.jsx
+++ src/components/ParallaxBackground.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import { motion, useScroll, useSpring, useTransform } from 'motion/react';
 
 const ParallaxBackground = () => {
+const { scrollYProgress } = useScroll();
+const x = useSpring(scrollYProgress, { damping:50 });
+const mountain3y = useTransform(x, [0, 0.5], ["0%", "70%"]);
+const planetsX = useTransform(x, [0, 0.5], ["0%", "-20%"]);
+const mountain2y = useTransform(x, [0, 0.5], ["0%", "30%"]);
+const mountain1y = useTransform(x, [0, 0.5], ["0%", "0%"]);
+
   return (
     <section className="absolute inset-0 bg-black/40">
         <div className="relative h-screen overflow-y-hidden">
@@ -13,35 +20,39 @@ const ParallaxBackground = () => {
                 backgroundSize: "cover",        
                 }} />
             {/* mountain layer 3 */}
-            <div className="absolute inset-0 -z-40"
+            <motion.div className="absolute inset-0 -z-40"
             style={{ 
                 backgroundImage: "url('/assets/mountain-3.png')",
                 backgroundPosition: "bottom",
-                backgroundSize: "cover",        
+                backgroundSize: "cover",
+                y: mountain3y,    
                 }}/>
             {/* planets */}
-            <div 
+            <motion.div 
             className="absolute inset-0 -z-30"
             style={{ 
                 backgroundImage: "url('/assets/planets.png')",
                 backgroundPosition: "bottom",
-                backgroundSize: "cover",        
+                backgroundSize: "cover",
+                x: planetsX,
                 }}/>
             {/* mountain layer 2 */}
-            <div 
+            <motion.div 
             className="absolute inset-0 -z-20"
             style={{ 
                 backgroundImage: "url('/assets/mountain-2.png')",
                 backgroundPosition: "bottom",
-                backgroundSize: "cover",        
+                backgroundSize: "cover",
+                y: mountain2y,        
                 }}/>
             {/* mountain layer 1 */}
-            <div 
+            <motion.div 
             className="absolute inset-0 -z-10"
             style={{ 
                 backgroundImage: "url('/assets/mountain-1.png')",
                 backgroundPosition: "bottom",
-                backgroundSize: "cover",        
+                backgroundSize: "cover",
+                y: mountain1y,
                 }}/>
         </div>
     </section>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,6 +2,10 @@
 import React from "react";
 import HeroText from "../components/HeroText";
 import ParallaxBacckground from "../components/ParallaxBackground";
+import { Canvas } from "@react-three/fiber";
+import { Astronaut } from "../components/Astronaut";
+import { OrbitControls } from "@react-three/drei";
+
 
 const Hero = () => {
   return (
@@ -9,6 +13,12 @@ const Hero = () => {
 md:items-start md:justify-start min-h-screen overflow-hidden c-space">
   <HeroText /> 
   <ParallaxBacckground />
+  <figure className="absolute inset-0" style={{ width: "100vw", height: "100vh" }}>
+    <Canvas>
+     <Astronaut />
+     <OrbitControls />
+    </Canvas>
+  </figure>
 </section>
   );
 };
