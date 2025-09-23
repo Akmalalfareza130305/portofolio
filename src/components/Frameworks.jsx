1811fa42abac3edfbb8a0e9711c401607633eac7
Akmalalfareza130305 <akmalalfareza20@gmail.com>
2025-09-23 12:59:52 +0700

    Project selesai dan sudah direvisi 2.1

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -22,12 +22,12 @@
       <div className="relative flex h-[15rem] w-full flex-col items-center justify-center">
         <OrbitingCircles iconSize={40}>
           {skills.map(( skills, index ) => (
-            <Icon key={index} src={`/public/assets/logos/${skills}.svg`}/>
+            <Icon key={index} src={`/assets/logos/${skills}.svg`}/>
             ))}
         </OrbitingCircles>
         <OrbitingCircles iconSize={25} radius={100} reverse speed={2}>
                   {skills.reverse().map(( skills, index ) => (
-            <Icon key={index} src={`/public/assets/logos/${skills}.svg`}/>
+            <Icon key={index} src={`/assets/logos/${skills}.svg`}/>
             ))}
         </OrbitingCircles>
       </div>
